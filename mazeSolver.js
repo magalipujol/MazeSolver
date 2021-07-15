f85e79864adcbc79e19b147ff0a57284ee79f8c0
Magali Pujol <magalicpujol@gmail.com>
2021-07-15 10:39:30 -0300

    tried to add path to maze

--- mazeSolver.js
+++ mazeSolver.js
@@ -1,13 +1,15 @@
 //TODO seleccionar un array aleatorio de mazes object
 let maze = mazes.maze1.matrix
 //TODO path tiene que tener la posicion de entrada y salida ya puestas
-let path = []
+let path = [[actualRow, actualColumn]]
 let deadEnds = []
 // TODO check if I need a newPosition array, or if only a camino array it's okay
 let newPosition = []
 let actualRow = mazes.maze1.starRow
 let actualColumn = mazes.maze1.startColumn
 
+// if the position is the same as the exit, the function returns the path
+// if not, the position changes, and check again if it's the exit
 function solveMaze() {
     if (maze[actualRow][actualColumn] == s) {
         return path
@@ -18,11 +20,12 @@ function solveMaze() {
     }
 }
 
-//this doesn't work :(
+// I check if I can move right, and if so, I move, else I try doing down, left and up
+// If all four positions have a wall, doesn't exist or are on the path, that position is a 
+// dead end
 function move() {
     // TODO esto puede entrar en un loop infinito en el que no encuentra dead ends
     // siempre va a poder volver a donde vino
-    console.log(actualRow + ", " + actualColumn)
     if (canMoveRight()) {
         moveRight()
         console.log("derecha")
@@ -37,6 +40,7 @@ function move() {
         console.log("arriba ")
     } else {
         // TODO chequear que pasa cuando entra acá
+        console.log("wtf")
         deadEnds.push([actualRow, actualColumn])
     }
     return newPosition
@@ -78,13 +82,21 @@ function moveUp() {
     addPositionToPath()
 }
 
+// this function adds the current position to an array called Path, where
+// I store the path to reach the exit
+// Alto, it calls another function, addPathToMaze 
 function addPositionToPath() {
     newPosition = [actualRow, actualColumn]
+    // addPathToMaze()
     return path.push([actualRow, actualColumn])
 }
 
+// When I uncomment this, "too much recursion" :(
 // this function changes the number of the positions of the path
 // to avoid going back through the path
-function addPathToMaze() {
+// function addPathToMaze() {
+//     maze[actualRow][actualColumn] = 2
+//     return maze
+// }
 
-}
+
