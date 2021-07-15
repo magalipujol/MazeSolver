f3d16ebe3fdce18d55d6dd7097d5778daf69bee4
Magali Pujol <magalicpujol@gmail.com>
2021-07-15 13:13:11 -0300

    started with design

---
+++ main.js
@@ -0,0 +1,7 @@
+// for (let i = 0; i < mazes.maze1.matrix.length; i++) {
+//     var div = document.createElement('div')
+//     div.innerHTML += mazes.maze1.matrix.length[i]
+// }
+
+// document.getElementById("maze").appendChild(div)
+
